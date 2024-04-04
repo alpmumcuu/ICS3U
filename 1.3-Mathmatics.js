5185f1eecfbacb861863e18e3a68d18e675d9aab
alpmumcuu <alp.mumcu@stu.ocsb.ca>
2024-04-04 14:26:40 -0400

    Update 1.3-Mathmatics.js

---
+++ 1.3-Mathmatics.js
@@ -0,0 +1,35 @@
+// Part 1
+
+// declare the starting variables
+let celsius = 100;
+let fahrenheit = 0;
+
+// celcius to farenheight formula
+fahrenheit = (celsius * 9/5) + 32;
+
+//print 
+console.log(fahrenheit)
+
+// Part 2
+// change the fahrenehit to 77 as asked
+fahrenheit = 77;
+
+// use celcius to fahrenheit
+celsius = (fahrenheit - 32) * 5/9;
+
+// print the result
+console.log(celsius);
+
+// Part 3
+console.log("77 degrees Fahrenheit is",celsius,"degrees celsius.");
+
+// Part 4
+
+x1 = -4;
+y1 = 5;
+x2 = 6;
+y2 = 0;
+
+m = (y2 - y1)/(x2 - x1);
+
+console.log("The slope is equal to",m);
