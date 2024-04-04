6460e81130eef77dd792a7f69c065ce34a76a020
alpmumcuu <alp.mumcu@stu.ocsb.ca>
2024-04-04 13:48:41 -0400

    assingment

---
+++ Unit01/1.1-Output.js
@@ -0,0 +1,15 @@
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+console.log("\n\n");
+
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080");
+
+console.log("\n\n");
+
+console.log("x\to\tx");
+console.log("o\tx\to");
+console.log("x\to\tx");
