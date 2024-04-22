a5825aefe8727becdc3cc36b84726f8c9a514ef4
alpmumcuu <alp.mumcu@stu.ocsb.ca>
2024-04-22 13:55:32 -0400

    assingment

--- 2.2-ReturnStatement.js
+++ 2.2-ReturnStatement.js
@@ -1,18 +1,18 @@
 function is_it_the_weekend(day){
     if ((day == 1 || 7)){
-        return True;
+        return true;
     }
     else{
-        return False;
+        return false;
     }
 }
 
 function evNineIble(value){
     if ((value%9==0)&&(value%2==0)){
-        return True;
+        return true;
     }
     else{
-        return False;
+        return false;
     }
 }
 
@@ -28,13 +28,14 @@ function area_cylinder(r, h){
     return(`Surface area of the Cylinder: ${area_circle(r)}*2 + ${area_label(r,h)}`)
 }
 
-function ghost_watch(polter, wolter)
+function ghost_watch(polter, wolter){
     if ((polter == "angry" && wolter == "angry") || (polter == "laughing" && wolter == "laughing")){
-        return True;
+        return true;
     }
     if ((polter == 0 && wolter == 0) || (polter == 1 && wolter == 1)){
-        return True;
+        return true;
     }
     if (polter && wolter){
-        return True;
+        return true;
     }
+}
